3c3811f3a5baa88785c9e11dd4d11a80698d23e2
Red71300 <moon_shadow_s@hotmail.fr>
2020-12-29 14:17:09 +0100

    Auto écriture classe Cereales + génération alétoire de nombres

--- src/app/lot-cereales/lot-cereales.component.ts
+++ src/app/lot-cereales/lot-cereales.component.ts
@@ -7,13 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LotCerealesComponent implements OnInit {
 
-  /*numLot : String = "Lol";
-  type : String = "";
-  poids : String = "";
-  tauxHumidite : String = "";
-  qualite : String = "";
-  origine : String = "";*/
-  constructor(public numLot: String, public type: String, public poids: Number, public tauxHumidite: Number, public qualite: Number, public origine: String) { 
+  numLot : String = Math.floor(Math.random() * 10).toString();
+  type : String = "Nesquik";
+  poids : String = (Math.floor(Math.random() * 30) + 5).toString();
+  tauxHumidite : String = Math.floor(Math.random() * 100).toString();;
+  qualite : String = "Bonne";
+  origine : String = "Paris";
+  constructor(/*public numLot: String, public type: String, public poids: Number, public tauxHumidite: Number, public qualite: Number, public origine: String*/) { 
     
   }
 
--- src/app/services/lot.service.ts
+++ src/app/services/lot.service.ts
@@ -7,28 +7,9 @@ export class LotService
 
     constructor()
     {
-        this.lots.push(new LotCerealesComponent('1','Chocapic',1,1,1,'1'));
-        this.lots.push(new LotCerealesComponent('2','Nesquik',2,2,2,'2'));
-        this.lots.push(new LotCerealesComponent('3','Crunch',3,3,3,'3'));
-        this.lots.push(new LotCerealesComponent('4','Lion',4,4,4,'4'));
+        this.lots.push(new LotCerealesComponent(/*'1','Chocapic',1,1,1,'1'*/));
+        this.lots.push(new LotCerealesComponent(/*'2','Nesquik',2,2,2,'2'*/));
+        this.lots.push(new LotCerealesComponent(/*'3','Crunch',3,3,3,'3'*/));
+        this.lots.push(new LotCerealesComponent(/*'4','Lion',4,4,4,'4'*/));
     }
-
-    /*lots = [
-        {
-            numLot: '1',
-            type: 'lion',
-            poids: '20 kg',
-            tauxHumidite: 'ambiant',
-            qualite: 'bonne',
-            origine: 'saint-etienne'
-        },
-        {
-            numLot: '12',
-            type: 'chocapic',
-            poids: '10 kg',
-            tauxHumidite: 'élevé',
-            qualite: 'mauvaise',
-            origine: 'paris'
-        }
-    ]*/
 }
