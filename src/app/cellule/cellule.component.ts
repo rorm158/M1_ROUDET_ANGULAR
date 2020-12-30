f38ef52a6d72a9ef46cfc12ad05c4a4047387aa7
DaraenAltWhen <Lukas_Barthelemy@etu.u-bourgogne.fr>
2020-12-30 17:04:21 +0100

    création de cellule

---
+++ src/app/cellule/cellule.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cellule',
+  templateUrl: './cellule.component.html',
+  styleUrls: ['./cellule.component.css']
+})
+export class CelluleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
