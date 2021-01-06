b60aebaa54c1e8a6626e63c60992deedca07d282
Red71300 <moon_shadow_s@hotmail.fr>
2021-01-06 22:12:40 +0100

    Last development commit

--- src/app/lot-cereales/lot-cereales.component.ts
+++ src/app/lot-cereales/lot-cereales.component.ts
@@ -8,20 +8,22 @@ import { CamionComponent } from '../camion/camion.component';
 })
 export class LotCerealesComponent implements OnInit {
 
-  @Input() numLot : String ;
-  @Input() type : String = "Nesquik";
+  @Input() numLot : String;
+  @Input() type : String;
   @Input() poids : String;
   @Input() tauxHumidite : String;
   @Input() qualite : String;
-  @Input() origine : String = "Paris";
+  @Input() origine : String;
   @Input() analyse : boolean = false;
   @Input() nettoye : boolean = false;
   @Input() ajoute : boolean = false;
   @Input() index : number;
   types: string[] = ['Blé', 'Orge', 'Pois','Pois','Protéagineux','Colza'];
+  origines: string[] = ['Agriculteur 1', 'Agriculteur 2', 'Agriculteur 3','Agriculteur 4','Agriculteur 5'];
   
   constructor() {
     this.type = this.types[Math.floor(Math.random() * 5)];
+    this.origine = this.origines[Math.floor(Math.random() * 5)];
   }
 
   ngOnInit(): void {
